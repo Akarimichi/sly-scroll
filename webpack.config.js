3799853b0be2fcaadbd604af1ec9fb95999e1071
David Hartley <davidhartley@live.com>
2018-01-11 12:30:17 +0000

    Updated to work with webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'index.js')
+    },
+
+    output: {
+        libraryTarget: 'commonjs2',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/, 
+                exclude: /(node_modules|dist)/, 
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
